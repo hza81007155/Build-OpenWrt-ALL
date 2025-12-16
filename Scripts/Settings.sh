532a9a374de1f8a1671ae2d308ebc86cf621ea6e
hza81007155 <800755@qq.com>
2025-12-17 02:34:18 +0800

    Change default IP from 192.168.1.1 to 192.168.10.12
    Update default IP address in configuration scripts.

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -21,6 +21,10 @@ find ./feeds/luci/collections/ -type f -name "Makefile" -exec sed -i "s/luci-the
 # 修改默认IP地址
 #sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OWRT_IP/g" ./package/base-files/luci2/bin/config_generate
 
+# 设置默认ip
+sed -i 's/192.168.1.1/192.168.10.12/g' package/base-files/luci/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.12/g' package/base-files/files/bin/config_generate
+
 # 修改默认主机名
 #sed -i "s/hostname='.*'/hostname='$OWRT_NAME'/g" ./package/base-files/luci2/bin/config_generate
 

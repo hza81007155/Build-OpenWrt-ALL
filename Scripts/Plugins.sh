c154192cb734e0af4a01f67e8dfada9535e44468
hza81007155 <800755@qq.com>
2026-04-26 08:45:37 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -12,8 +12,8 @@ git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luc
 #git clone https://github.com/hza81007155/luci-app-partexp
 
 # 18.06 Argone theme
-git clone https://github.com/hza81007155/luci-theme-argone
-git clone https://github.com/hza81007155/luci-app-argone-config
+git clone -b 18.06 https://github.com/hza81007155/luci-theme-argon.git luci-theme-argon
+git clone -b 18.06 https://github.com/hza81007155/luci-app-argon-config.git luci-app-argon-config
 #git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 #git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-app-argon-config
 
